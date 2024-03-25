13f72edcde00eff873d143c49261880db7163c10
Bryan Durham <69902245+bryand4140@users.noreply.github.com>
2024-03-25 17:46:07 -0600

    Added blade drag / properties
    One function was added that calculates the blade drag as a function of air density and engine rotation in RPM. An additional script was added that calculates the tensile blade stiffness constant k_t.

---
+++ Blade_Drag.m
@@ -0,0 +1,22 @@
+function [D] = Blade_Drag(RPM, air_density)
+%{
+This function calculates the total drag on a single engine blade. 
+INPUTS
+    RPM = Engine rotational rate in rev/minute
+    density = air density in kg/m^3
+OUTPUTS
+    D = Blade Drag in N
+%}
+
+    %Blade Geometry and Properties:
+    c  = 4*(1/12)*(1/3.28); %Length averaged chord, [m]
+    l  = 0.925*(3.4044/2);  %Blade length corrected for hub radius, [m]
+    CD = 0.1;               %Assumed Drag Coefficient
+    
+    %Engine Parms.
+    omega = rpm_2_rads(RPM);
+    
+    %Calculations
+    S = l*c;     %Blade planform area, [m^2]
+    D = (1/6) * l^3 * CD * air_density * omega^2 * S; %Single Blade Drag
+end
---
+++ Blade_Properties.m
@@ -0,0 +1,21 @@
+clear;close all; clc; format shorteng
+
+t = 0.02;              %Chord averaged thickness, [m]
+c = 4*(1/12)*(1/3.28); %Length averaged chord, [m]
+l = 0.925*(3.4044/2);  %Blade length corrected for hub radius, [m]
+
+%Material properties
+rho_b = 2.0e3;   %Approx. carbon composite density, [kg/m^3]
+E     = 227.0e9; %Approx. Young's modulus for carbon fiber in tension, [N/m^2]
+
+%Calculations
+A = t*c;     %Length averaged cross sectional area, [m^2]
+V = t*c*l;   %Approximate blade volume, [m^3]
+S = l*c;     %Blade planform area, [m^2]
+m = rho*V;   %Mass of a single blade, [kg]
+k_t = E*A/l; %Spring constant for a single blade, [N/m]
+
+
+%Print Statements
+fprintf('Mass of a single blade = %0.4f kg\n',m)
+fprintf('Blade tensile spring constant = %0.4f MN/m\n',k_t/1e6)
